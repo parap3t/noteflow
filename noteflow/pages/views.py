ccca9e7a175cdb140339c559ce8819acc9e43ada
parap3t <d.parkhomenko2005@gmail.com>
2025-04-29 11:56:25 +0500

    Добавлено отображение страниц в файле pages/views.py

--- noteflow/pages/views.py
+++ noteflow/pages/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def notes(request):
+    return render(request, 'pages/notes.html')
