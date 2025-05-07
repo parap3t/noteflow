d27c70b65bf7a9f46d2c11c283a4280869d61288
parap3t <d.parkhomenko2005@gmail.com>
2025-05-07 11:49:52 +0500

    Добавлена модель Note в файл pages/models.py для хранения заметок пользователей

--- noteflow/pages/models.py
+++ noteflow/pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User  # Подключаем встроенную модель User
 
-# Create your models here.
+
+class Note(models.Model):
+    text = models.TextField()  # Поле для текста заметки
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Внешний ключ к пользователю
+
+
