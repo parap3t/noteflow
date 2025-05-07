12944caf5a7ce7d711ee407d7c2a6011d0a9ebdb
parap3t <d.parkhomenko2005@gmail.com>
2025-05-07 12:00:36 +0500

    Добавлена проверка авторизации и получение заметок текущего пользователя в функции notes (файл pages/views.py)

--- noteflow/pages/views.py
+++ noteflow/pages/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required  # Импортируем декоратор
 
-# Create your views here.
+from .models import Note
+
+
 def home(request):
-    return render(request, 'pages/home.html')
+    return render(request, 'pages/index.html')
 
 def about(request):
     return render(request, 'pages/about.html')
 
+
+@login_required
 def notes(request):
-    return render(request, 'pages/notes.html')
+
+    # Получаем все заметки текущего пользователя
+    user_notes = Note.objects.filter(user=request.user)
+
+    return render(request, 'pages/notes.html', {'notes': user_notes})
