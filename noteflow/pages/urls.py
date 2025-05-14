e44375c30ec60ceb845b4f0792211bcb98ccdddd
parap3t <d.parkhomenko2005@gmail.com>
2025-05-14 11:40:39 +0500

    Изменена функция views.register на views.register_view

--- noteflow/pages/urls.py
+++ noteflow/pages/urls.py
@@ -22,7 +22,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.login_view, name='login'),
-    path('register/', views.register, name="register"),
+    path('register/', views.register_view, name="register"),
     path('about/', views.about, name="about"),
     path('notes/', views.notes, name="notes")
 ]
