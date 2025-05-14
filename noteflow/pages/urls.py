3b871d5b1b0bd8d059fa476718d222c1c4e06542
parap3t <d.parkhomenko2005@gmail.com>
2025-05-14 11:36:50 +0500

    Изменена функция views.login на views.login_view

--- noteflow/pages/urls.py
+++ noteflow/pages/urls.py
@@ -21,7 +21,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
     path('register/', views.register, name="register"),
     path('about/', views.about, name="about"),
     path('notes/', views.notes, name="notes")
