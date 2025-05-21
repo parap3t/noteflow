c8a12a6be5266ad2dcd88bec8210dba1cf482755
parap3t <d.parkhomenko2005@gmail.com>
2025-05-21 22:22:23 +0500

    Вынесен список разрешенных хостов в окружение .env

--- noteflow/noteflow/settings.py
+++ noteflow/noteflow/settings.py
@@ -24,16 +24,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'pages', 'static', 'pages'),  # Для приложения pages
 ]
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')  # Извлекаем список разрешенных хостов
 
 
 # Application definition
