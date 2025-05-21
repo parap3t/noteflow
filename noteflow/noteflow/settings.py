f3dcff54c8eb57871f24477c47864f17f1f2f2d8
parap3t <d.parkhomenko2005@gmail.com>
2025-05-21 22:18:35 +0500

    Спрятан SECRET_KEY в окружении .env в файле settings.py

--- noteflow/noteflow/settings.py
+++ noteflow/noteflow/settings.py
@@ -9,21 +9,29 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import os
 
+from dotenv import load_dotenv
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Загружаем переменные из файла .env
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent  # Корень проекта
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'pages', 'static', 'pages'),  # Для приложения pages
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x!qqml00u1h3hoh)%n)7b7s7pn5*e_y!hd4s^jgezcjwlb+#y0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -54,7 +62,7 @@ ROOT_URLCONF = 'noteflow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
